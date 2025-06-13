adddaa997dd34b55ebe12a492ba3ca7babdd6411
JuanD <dosquisd123@gmail.com>
2025-06-12 20:25:01 -0500

    fix: rename env variables (mistype)

--- typechecking/scripts/deploy_rabbitmq_docker.sh
+++ typechecking/scripts/deploy_rabbitmq_docker.sh
@@ -3,6 +3,6 @@ source .env
 docker run -d --name typechecking-rabbitmq \
   -p "${RABBITMQ_PORT}:5672" \
   -p 15672:15672 \
-  -e RABBITMQ_DEFAULT_USER=${RABBITMQ_DEFAULT_USER} \
-  -e RABBITMQ_DEFAULT_PASS=${RABBITMQ_DEFAULT_PASS} \
+  -e RABBITMQ_DEFAULT_USER=${RABBITMQ_USER} \
+  -e RABBITMQ_DEFAULT_PASS=${RABBITMQ_PASSWORD} \
   rabbitmq:4-management
