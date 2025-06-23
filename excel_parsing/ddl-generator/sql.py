77f0b06a7dbfbc728cc4cbb965034df36a328dbd
JuanD <dosquisd123@gmail.com>
2025-06-22 20:38:09 -0500

    refactor: rewrite sql generator

--- excel_parsing/ddl-generator/sql.py
+++ excel_parsing/ddl-generator/sql.py
@@ -1,29 +1,16 @@
 from typing import List
-from dtypes import CellRangeMapsOutput, AllOutputs, SingleOutput, ComplexOutput
+from dtypes import AllOutputs
+
+FUNCTION_SQL_MAP = {
+    "SUM": lambda args: " + ".join(args[0]["columns"]),
+    "IF": lambda args: f"CASE WHEN {args[0]['sql']} THEN {args[1]['sql']} ELSE {args[2]['sql']} END",
+    "AND": lambda args: " AND ".join(arg["sql"] for arg in args),
+    # Add more functions here
+}
 
 
 def get_sql_from_function(func_name: str, args: List[AllOutputs]) -> str:
-    if func_name == "SUM":
-        # Here we assume that references are in the form of "A1", "B1", etc.
-        args: CellRangeMapsOutput = args[0]
-        cols = args["columns"]
-        sql = " + ".join(cols)
-        return sql
-
-    if func_name == "IF":
-        condition: ComplexOutput = args[0]
-        true_value: SingleOutput = args[1]
-        false_value: SingleOutput = args[2]
-        sql = (
-            f"CASE WHEN {condition['sql']} THEN {true_value['sql']} "
-            + f"ELSE {false_value['sql']} END"
-        )
-        return sql
-
-    if func_name == "AND":
-        sql = " AND ".join(arg["sql"] for arg in args)
-        return sql
-
-    # Add more function mappings here as needed
-
-    return f"UNSUPPORTED_FUNCTION({func_name})"
+    func = FUNCTION_SQL_MAP.get(func_name.upper())
+    if not func:
+        return f"UNSUPPORTED_FUNCTION({func_name})"
+    return func(args)
