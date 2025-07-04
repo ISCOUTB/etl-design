90f534e1009c8e8ec9304ae1fb220ca94a2fd80c
JuanD <dosquisd123@gmail.com>
2025-07-04 14:15:19 -0500

    fix: bad syntax

--- excel-parsing/excel-reader/scripts/formula_parser_clients.sh
+++ excel-parsing/excel-reader/scripts/formula_parser_clients.sh
@@ -1,5 +1,5 @@
-python3 -m grpc_tools.protoc dtypes.proto formula_parser.proto
-    --proto_path ../proto/
-    --python_out ./src/clients/formula_parser/
-    --pyi_out ./src/clients/formula_parser/
-    --grpc_python_out ./src/clients/formula_parser/
+python3 -m grpc_tools.protoc \
+  dtypes.proto formula_parser.proto \
+  --proto_path ../proto/ --python_out ./src/clients/formula_parser/ \
+  --pyi_out ./src/clients/formula_parser/ \
+  --grpc_python_out ./src/clients/formula_parser/
