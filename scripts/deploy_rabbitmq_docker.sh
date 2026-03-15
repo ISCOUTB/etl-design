39d21e28f2f917505c69bb00ab3a81a22febbb21
JuanD <dosquisd123@gmail.com>
2026-03-15 18:36:44 -0500

    refactor: add a named volume for rabbitmq local deployment with docker; deploy scripts for development were moved to root

--- scripts/deploy_rabbitmq_docker.sh
+++ scripts/deploy_rabbitmq_docker.sh
@@ -5,4 +5,5 @@ docker run -d --name typechecking-rabbitmq \
   -p 15672:15672 \
   -e RABBITMQ_DEFAULT_USER=${RABBITMQ_USER} \
   -e RABBITMQ_DEFAULT_PASS=${RABBITMQ_PASSWORD} \
+  -v typechecking_rabbitmq_data:/var/lib/rabbitmq \
   rabbitmq:4-management
