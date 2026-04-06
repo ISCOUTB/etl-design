df3cb48d7700b8211c9d5f2d05dc396e75445408
JuanD <pereznjuandiego@gmail.com>
2026-04-06 16:27:38 -0500

    feat(tasks): enhance task retrieval logic to handle optional task types and improve response handling

--- apps/backend/api/src/api/routes/healthcheck.py
+++ apps/backend/api/src/api/routes/healthcheck.py
@@ -14,11 +14,12 @@ async def healthcheck(
     Healthcheck endpoint to verify that the API is running.
     This endpoint can be used by monitoring tools to check the health of the API.
     """
-    mongo_status = (await database_client.mongo_ping_async())["pong"]
-    redis_status = (await database_client.redis_ping_async())["pong"]
+    mongo_response = database_client.mongo_ping_async()
+    redis_response = database_client.redis_ping_async()
+    mongo_status, redis_status = await mongo_response, await redis_response
 
     return HealthCheckResponse(
-        mongo_status=mongo_status,
-        redis_status=redis_status,
+        mongo_status=mongo_status["pong"],
+        redis_status=redis_status["pong"],
         message_queue=publisher._channel.is_open,
     )
--- apps/backend/api/src/api/routes/tasks.py
+++ apps/backend/api/src/api/routes/tasks.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter
 from proto_utils.database import dtypes
 
 from src.api.deps import CurrentUser, DatabaseClientDep, IdempotencyServiceDep
-from src.core.constants import VALIDATION_TASK
+from src.core.constants import INSERTION_TASK, VALIDATION_TASK
 from src.exceptions import ForbiddenException, TaskNotFoundException
 from src.models import Project
 from src.services.permissions import Action, ModelKeys, PermissionService
@@ -18,7 +18,7 @@ async def get_task_status(
     current_user: CurrentUser,
     database_client: DatabaseClientDep,
     idempotency_service: IdempotencyServiceDep,
-    task: str = VALIDATION_TASK,
+    task: Optional[str] = None,
 ) -> dtypes.ApiResponse:
     """Get the status of a long-running task by its ID."""
     has_permission = PermissionService.has_permission(
@@ -29,9 +29,20 @@ async def get_task_status(
     if not has_permission:
         raise ForbiddenException()
 
-    cached_response = await database_client.get_task_id_async(
-        dtypes.GetTaskIdRequest(task_id=task_id, task=task)
-    )
+    if task is None:
+        # If task type is not specified, check for both validation and insertion tasks
+        cached_response = await database_client.get_task_id_async(
+            dtypes.GetTaskIdRequest(task_id=task_id, task=VALIDATION_TASK)
+        )
+        if not cached_response["found"] or cached_response["value"] is None:
+            cached_response = await database_client.get_task_id_async(
+                dtypes.GetTaskIdRequest(task_id=task_id, task=INSERTION_TASK)
+            )
+    else:
+        cached_response = await database_client.get_task_id_async(
+            dtypes.GetTaskIdRequest(task_id=task_id, task=task)
+        )
+
     if not cached_response["found"] or cached_response["value"] is None:
         # If not found in redis/mongo, search in the main database (PostgreSQL)
         # as a fallback
@@ -55,7 +66,7 @@ async def list_tasks(
     database_client: DatabaseClientDep,
     project_id: str,
     table_name: str,
-    task: str = VALIDATION_TASK,
+    task: Optional[str] = None,
 ) -> List[dtypes.ApiResponse]:
     """List all tasks associated with a specific import name."""
     has_permission = PermissionService.has_permission(
@@ -67,12 +78,34 @@ async def list_tasks(
     if not has_permission:
         raise ForbiddenException()
 
-    response = await database_client.get_tasks_by_import_name_async(
-        dtypes.GetTasksByImportNameRequest(
-            import_name=f"{project_id}__{table_name}", task=task
+    if task is None:
+        print(task)
+        response_validation_tasks = database_client.get_tasks_by_import_name_async(
+            dtypes.GetTasksByImportNameRequest(
+                import_name=f"{project_id}__{table_name}", task=VALIDATION_TASK
+            )
         )
-    )
 
-    tasks = response.get("tasks") or []
+        response_insertion_tasks = database_client.get_tasks_by_import_name_async(
+            dtypes.GetTasksByImportNameRequest(
+                import_name=f"{project_id}__{table_name}", task=INSERTION_TASK
+            )
+        )
+
+        response_validation_tasks, response_insertion_tasks = (
+            await response_validation_tasks,
+            await response_insertion_tasks,
+        )
+
+        validation_tasks = response_validation_tasks.get("tasks") or []
+        insertion_tasks = response_insertion_tasks.get("tasks") or []
+        tasks = validation_tasks + insertion_tasks
+    else:
+        response = await database_client.get_tasks_by_import_name_async(
+            dtypes.GetTasksByImportNameRequest(
+                import_name=f"{project_id}__{table_name}", task=task
+            )
+        )
+        tasks = response.get("tasks") or []
 
     return tasks
