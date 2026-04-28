28e032d0f914272d0cd2e17e9431484c74284ea9
JuanD <dosquisd123@gmail.com>
2026-04-28 17:46:01 -0500

    build: regenerate files generated via protoc, and update all dependencies across the project in order to use the grpcio 1.80.0

--- packages/proto-utils/proto-utils-py/src/proto_utils/generated/parsers/formula_parser_pb2_grpc.py
+++ packages/proto-utils/proto-utils-py/src/proto_utils/generated/parsers/formula_parser_pb2_grpc.py
@@ -8,7 +8,7 @@ import warnings
 
 from . import formula_parser_pb2 as parsers_dot_formula__parser__pb2
 
-GRPC_GENERATED_VERSION = '1.78.0'
+GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
